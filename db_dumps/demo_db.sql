c47d31273551de303a16101ba6d312f4d1ccde3e
Shalitha Suranga <shalithasuranga@gmail.com>
2023-10-03 18:24:28 +0530

    Update db dumps

--- db_dumps/demo_db.sql
+++ db_dumps/demo_db.sql
@@ -2,17 +2,80 @@ CREATE DATABASE IF NOT EXISTS demo_db;
 
 USE demo_db;
 
+CREATE TABLE location (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    code CHAR(2) NOT NULL,
+    name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE customer (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) DEFAULT NULL,
-  location_id INT(5) DEFAULT NULL,
-  PRIMARY KEY (id)
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    location_id INT(5) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (location_id) REFERENCES location(id)
 );
 
 DESCRIBE customer;
 
-INSERT INTO customer(name, location_id) VALUES
-  ('John Doe',1),
-  ('Ann',1),
-  ('Tony',1),
-  ('Curl Johns',2);
+INSERT INTO location (code, name) VALUES
+('AL','Alabama'),
+('AK','Alaska'),
+('AS','American Samoa'),
+('AZ','Arizona'),
+('AR','Arkansas'),
+('CA','California'),
+('CO','Colorado'),
+('CT','Connecticut'),
+('DE','Delaware'),
+('DC','District of Columbia'),
+('FM','Federated States of Micronesia'),
+('FL','Florida'),
+('GA','Georgia'),
+('GU','Guam'),
+('HI','Hawaii'),
+('ID','Idaho'),
+('IL','Illinois'),
+('IN','Indiana'),
+('IA','Iowa'),
+('KS','Kansas'),
+('KY','Kentucky'),
+('LA','Louisiana'),
+('ME','Maine'),
+('MH','Marshall Islands'),
+('MD','Maryland'),
+('MA','Massachusetts'),
+('MI','Michigan'),
+('MN','Minnesota'),
+('MS','Mississippi'),
+('MO','Missouri'),
+('MT','Montana'),
+('NE','Nebraska'),
+('NV','Nevada'),
+('NH','New Hampshire'),
+('NJ','New Jersey'),
+('NM','New Mexico'),
+('NY','New York'),
+('NC','North Carolina'),
+('ND','North Dakota'),
+('MP','Northern Mariana Islands'),
+('OH','Ohio'),
+('OK','Oklahoma'),
+('OR','Oregon'),
+('PW','Palau'),
+('PA','Pennsylvania'),
+('PR','Puerto Rico'),
+('RI','Rhode Island'),
+('SC','South Carolina'),
+('SD','South Dakota'),
+('TN','Tennessee'),
+('TX','Texas'),
+('UT','Utah'),
+('VT','Vermont'),
+('VI','Virgin Islands'),
+('VA','Virginia'),
+('WA','Washington'),
+('WV','West Virginia'),
+('WI','Wisconsin'),
+('WY','Wyoming');
